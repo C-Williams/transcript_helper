c02759b795f3cc37eb7c49484b7298644c1c62c0
Chris Williams <christopher@flowkey.com>
2024-07-27 20:37:57 +0200

    add convert method directly. add in colors. fix file pathing

--- main.py
+++ main.py
@@ -2,10 +2,16 @@ import argparse
 import os
 import warnings
 import torch
+from pydub import AudioSegment
+from pydub.utils import mediainfo
 from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
 
-from convert import convert_to_wav
-
+TEXT_COLORS = {
+    'RED': '\033[91m',
+    'GREEN': '\033[92m',
+    'YELLOW': '\033[93m',
+    'END': '\033[0m'
+}
 # Redirect warnings to /dev/null
 with open(os.devnull, 'w') as f:
     warnings.simplefilter("ignore")
@@ -21,7 +27,7 @@ class TranscriptHelper:
         device = "cuda:0" if torch.cuda.is_available() else "cpu"
         torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 
-        model_id = "openai/whisper-base.en"
+        model_id = "openai/whisper-base"
 
         model = AutoModelForSpeechSeq2Seq.from_pretrained(
             model_id, torch_dtype=torch_dtype, use_safetensors=True
@@ -49,13 +55,12 @@ class TranscriptHelper:
             transcripted_files = self.read_transcripted_files(self.transcripted_files_file)
 
             new_files = [f for f in audio_files if f not in transcripted_files]
-
             for audio_file in new_files:
                 try:
-                    transcribed_file = self.transcribe_file()
+                    transcribed_file = self.transcribe_file(audio_file)
                     transcripted_files.add(transcribed_file)
                 except Exception as e:
-                    print(f"Error transcribing {audio_file}: {e}")
+                    print(f"{TEXT_COLORS['RED']}Error transcribing {audio_file}: {e}{TEXT_COLORS['END']}")
 
             self.write_transcripted_files(self.transcripted_files_file, transcripted_files)
         else:
@@ -64,51 +69,46 @@ class TranscriptHelper:
             transcripted_files.add(transcribed_file)
             self.write_transcripted_files(self.transcripted_files_file, transcripted_files)
 
-
-    def escape_path(self, path):
-        return path.replace(" ", "\\ ")
-
-    def transcribe_file(self):
+    def transcribe_file(self, file):
         delete_wav = False
-        wav_file = self.file
+        wav_file = file
 
-        if not self.file.endswith('.wav'):
-            wav_file = convert_to_wav(self.file, os.path.dirname(self.file))
+        if not file.endswith('.wav'):
+            wav_file = self.convert_to_wav(file, os.path.dirname(file))
             if wav_file:
                 delete_wav = True
             else:
-                print(f"Failed to convert {self.file} to WAV format")
+                print(f"{TEXT_COLORS['RED']}Failed to convert {file} to WAV format{TEXT_COLORS['END']}")
                 exit(1)
 
-        print(f"Transcribing file: {wav_file}")
+        print(f"{TEXT_COLORS['GREEN']}Transcribing file: {wav_file}{TEXT_COLORS['END']}")
         result = self.pipe(wav_file)
         if self.print_transcription:
             print(result["text"])
 
-        txt_file = os.path.join("..", "Movies", "Meeting transcriptions", os.path.basename(self.file).rsplit('.', 1)[0] + '.txt')
+        txt_file = os.path.join("..", "Movies", "Meeting transcriptions", os.path.basename(file).rsplit('.', 1)[0] + '.txt')
         os.makedirs(os.path.dirname(txt_file), exist_ok=True)
 
         with open(txt_file, 'w') as f:
             f.write(result["text"])
-        print(f"Transcription saved to {txt_file}")
+        print(f"{TEXT_COLORS['GREEN']}Transcription saved to {txt_file}{TEXT_COLORS['END']}")
 
         if delete_wav:
             os.remove(wav_file)
-            print(f"Deleted temporary wav file: {wav_file}")
 
         if self.delete_file:
-            print("Here is a sample of the transcription:")
+            print(f"{TEXT_COLORS['YELLOW']}Here is a sample of the transcription:{TEXT_COLORS['END']}")
             print(result["text"][:100])
-            answer = input("Would you like to delete the original file? (y/n): ")
+            answer = input(f"{TEXT_COLORS['YELLOW']}Would you like to delete the original file? (y/n): {TEXT_COLORS['END']}")
             if answer.lower() == 'y':
-                os.remove(self.file)
-                print(f"Deleted original file: {self.file}")
+                os.remove(file)
+                print(f"Deleted original file: {file}")
             else:
-                print(f"Original file kept: {self.file}")
-        return self.file
+                print(f"Original file kept: {file}")
+        return file
 
     def get_audio_files(self, directory):
-        supported_formats = ['.mkv', '.mp3', '.flac', '.aac', '.ogg', '.wma']
+        supported_formats = ['.mkv', '.mp3', '.flac', '.aac', '.ogg', '.wma', '.m4a', '.aiff', '.ape', '.au', '.mpc', '.ra', '.rm', '.sln', '.vqf', '.w64', '.wv', '.webm', '.opus']
         return [os.path.join(directory, f) for f in os.listdir(directory) if os.path.splitext(f)[1].lower() in supported_formats]
 
     def read_transcripted_files(self, file):
@@ -118,10 +118,37 @@ class TranscriptHelper:
             return set(f.read().splitlines())
 
     def write_transcripted_files(self, file, transcripted_files):
-        print(f"Writing transcripted files to {file}")
         with open(file, 'w') as f:
             for file in transcripted_files:
                 f.write(file + '\n')
+
+    def convert_to_wav(self, file_path, output_directory):
+        """
+        Converts an audio file to WAV format.
+
+        Parameters:
+        - file_path: str, path to the input audio file.
+        - output_directory: str, directory where the output WAV file will be saved.
+        """
+        if not os.path.isfile(file_path):
+            print(f"{TEXT_COLORS['RED']}File not found: {file_path}{TEXT_COLORS['END']}")
+            return None
+
+        file_info = mediainfo(file_path)
+        if 'format_name' not in file_info:
+            print(f"Not a valid audio file: {file_path}")
+            return None
+
+        base_name = os.path.splitext(os.path.basename(file_path))[0]
+        output_path = os.path.join(output_directory, f"{base_name}.wav")
+
+        try:
+            audio = AudioSegment.from_file(file_path)
+            audio.export(output_path, format="wav")
+            return output_path
+        except Exception as e:
+            print(f"Failed to convert {file_path}: {e}")
+            return None
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Transcribe audio files using Whisper. Accepts various audio file formats.")
@@ -132,11 +159,11 @@ if __name__ == "__main__":
     args = parser.parse_args()
     if args.print:
         if args.print.lower() != 'true':
-            print("Invalid argument for 'PRINT'. Expected 'true' or nothing.")
+            print(f"{TEXT_COLORS['RED']}Invalid argument for 'PRINT'. Expected 'true' or nothing.{TEXT_COLORS['END']}")
             exit(1)
     if args.delete:
         if args.delete.lower() != 'true':
-            print("Invalid argument for 'DELETE'. Expected 'true' or nothing.")
+            print(f"{TEXT_COLORS['RED']}Invalid argument for 'DELETE'. Expected 'true' or nothing.{TEXT_COLORS['END']}")
             exit(1)
     helper = TranscriptHelper(args.file, args.print, args.delete)
     helper.process_files()
